7772c211ff867cae5c459f81b1932278b6c40441
Artur <artur.m99@wp.pl>
2021-03-08 23:07:59 +0100

    Add version 1.1

--- Lab1/app/src/main/java/org/zzpj/app/App.java
+++ Lab1/app/src/main/java/org/zzpj/app/App.java
@@ -2,10 +2,25 @@ package org.zzpj.app;
 
 import org.zzpj.lib.Person;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class App {
 
     public static void main( String[] args ) {
         Person person = new Person("Artur");
+        person.setName(getNameProperty());
         person.sayHello();
+    }
+
+    public static String getNameProperty() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = App.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("clientName");
     }
 }
